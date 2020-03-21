71135664db751f710099248868e20b92e50d7394
Murilo Henrique <oofleaoo@gmail.com>
2020-03-21 01:06:43 -0300

    save hero on a file with tests

--- cli/database.js
+++ cli/database.js
@@ -1,7 +1,8 @@
-const { readFile } = require('fs');
+const { readFile, writeFile } = require('fs');
 const { promisify } = require('util');
 
 const readFileAsync = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
 
 class Database {
   constructor() {
@@ -11,7 +12,19 @@ class Database {
     const archive = await readFileAsync(this.ARCHIVE_NAME, 'utf8');
     return JSON.parse(archive.toString());
   }
-  writeArchive(){}
+  async writeArchive(data){
+    await writeFileAsync(this.ARCHIVE_NAME, JSON.stringify(data));
+    return true;
+  }
+  async save(hero) {
+    const heros = await this.getArchiveData();
+    const id = hero.id <= 2 ? hero.id : Math.floor(Math.random() * (10e5 - 3) + 3);
+    const heroWithId = { ...hero, id };
+    const newHeros = heros.filter(item => item.id !== id);
+    newHeros.push(heroWithId);
+    const result = await this.writeArchive(newHeros);
+    return result;
+  }
   async search(id){
     const data = await this.getArchiveData();
     const dataFiltered = data.filter(item => {
--- cli/test.js
+++ cli/test.js
@@ -1,13 +1,24 @@
-const { deepEqual, ok } = require('assert');
-const database = require('./database');
+const { deepEqual, ok } = require("assert");
+const database = require("./database");
 
-const DEFAULT_ITEM_SAVE = { name: 'Flash', power: 'speed', id: 1 };
+const DEFAULT_ITEM_SAVE = { name: "Flash", power: "speed", id: 1 };
 
-describe('Hero Manipulation Suite', () => {
+describe("Hero Manipulation Suite", () => {
+  before(async () => {
+    await database.save(DEFAULT_ITEM_SAVE);
+  });
 
-  it('should search a hero using archives', async () => {
+  it("should search a hero using archives", async () => {
     const expected = DEFAULT_ITEM_SAVE;
     const [result] = await database.search(expected.id);
     deepEqual(result, expected);
-  })
+  });
+
+  it("should save a hero using archives", async () => {
+    const expected = { id: 2, name: "Batman", power: "Be a Rich" };
+    await database.save(expected);
+    const [actual] = await database.search(expected.id);
+
+    deepEqual(actual, expected);
+  });
 });
